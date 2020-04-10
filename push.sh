706e4892cd4041790e747580b2b3ca5baa813dc4
noralinlll <nora.lin@dji.com>
2020-04-10 10:20:31 +0800

    dd

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ message=$1
 # 更新 master
 git add .
 git commit -m "$message"
-#git push -f git@github.com:noralinlll/doc_test.git master
+git push -f git@github.com:noralinlll/doc_test.git master
 
 # 更新 gh-pages
 # cd docs/
