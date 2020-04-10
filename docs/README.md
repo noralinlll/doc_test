77faf09fab82a9cdc1acdd98c8163bba7c9835d9
noralinlll <nora.lin@dji.com>
2020-04-10 10:19:34 +0800

    dd

---
+++ docs/README.md
@@ -0,0 +1,8 @@
+# Headline
+
+> An awesome project.
+
+## Nora Test
+### 三级标题测试
+
+## 二级标题测试
---
+++ push.sh
@@ -0,0 +1,12 @@
+message=$1
+# 更新 master
+git add .
+git commit -m "$message"
+#git push -f git@github.com:noralinlll/doc_test.git master
+
+# 更新 gh-pages
+# cd docs/
+# git init
+# git add -A
+# git commit -m "$message"
+#git push -f git@github.com:noralinlll/doc_test.git  master:gh-pages
