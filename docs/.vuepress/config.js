099c1cd6b3c6440e5324ee5cf94d5d63282d1d82
nora.lin <nora.lin@dji.com>
2022-03-29 04:57:48 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  // 站点配置
+  lang: "zh-CN",
+  title: "你好， VuePress ！",
+  description: "这是我的第一个 VuePress 站点",
+
+  // 主题和它的配置
+  theme: "@vuepress/theme-default",
+  themeConfig: {
+    logo: "https://vuejs.org/images/logo.png",
+  },
+  extractHeaders: true,
+  toc: true,
+  themeConfig: {
+    // 侧边栏数组
+    // 所有页面会使用相同的侧边栏
+    sidebar: [
+      // SidebarItem
+      {
+        text: "Foo",
+        link: "/foo/",
+        children: [
+          // SidebarItem
+          {
+            text: "github",
+            link: "https://github.com",
+            children: [],
+          },
+          // 字符串 - 页面文件路径
+          "/foo/bar.md",
+        ],
+      },
+      // 字符串 - 页面文件路径
+      "/bar/README.md",
+    ],
+  },
+};
